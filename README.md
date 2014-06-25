f88b166858a71845184c3246d60fe3cf079b66bf
Jan Horava <czechinthemail@gmail.com>
2014-06-24 21:54:20 -0700

    added project files

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-Countdown.htm
-=============
+Add 100 seconds to the countdown whenever you wish and see how long you can avoid the countdown completion!
---
+++ js/countdown.js
@@ -0,0 +1,28 @@
+function add100(){
+    if(running){
+        document.getElementById('countdown').innerHTML =
+          parseInt(document.getElementById('countdown').innerHTML)
+          + 100;
+    }
+}
+
+function countdown(){
+    document.getElementById('countdown').innerHTML =
+      parseInt(document.getElementById('countdown').innerHTML)
+      - 1;
+    document.getElementById('score').innerHTML =
+      parseInt(document.getElementById('score').innerHTML)
+      + 1;
+
+    if(document.getElementById('countdown').innerHTML < 1){
+        clearInterval(interval);
+        running = 0;
+        document.getElementById('lose-message-hidden').style.display = 'block';
+    }
+}
+
+var interval = setInterval(
+  'countdown()',
+  1000
+);
+var running = 1;
