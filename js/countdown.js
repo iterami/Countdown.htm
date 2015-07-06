400315ad88590b5660eabba422576299c9e7cfdf
Jan Horava <czechinthemail@gmail.com>
2015-07-05 22:25:43 -0700

    added localStorage and reset button

--- js/countdown.js
+++ js/countdown.js
@@ -1,5 +1,6 @@
 function add(time){
-    if(!running){
+    if(!running
+      || parseInt(document.getElementById('countdown').innerHTML) <= 0){
         return;
     }
 
@@ -9,23 +10,55 @@ function add(time){
 }
 
 function countdown(){
+    var countdownvalue = parseInt(document.getElementById('countdown').innerHTML);
+
+    if(countdownvalue < 1){
+        running = false;
+        window.clearInterval(interval);
+        return;
+    }
+
     document.getElementById('countdown').innerHTML =
-      parseInt(document.getElementById('countdown').innerHTML)
+      countdownvalue
       - 1;
     document.getElementById('score').innerHTML =
       parseInt(document.getElementById('score').innerHTML)
       + 1;
 
-    if(document.getElementById('countdown').innerHTML < 1){
-        running = false;
-        window.clearInterval(interval);
-    }
+    window.localStorage.setItem(
+      'Countdown.htm-countdown',
+      document.getElementById('countdown').innerHTML
+    );
+    window.localStorage.setItem(
+      'Countdown.htm-score',
+      document.getElementById('score').innerHTML
+    );
 }
 
+function reset(){
+    if(!window.confirm('Reset?')){
+        return;
+    }
+
+    document.getElementById('countdown').innerHTML = 10;
+    document.getElementById('score').innerHTML = 0;
+
+    window.localStorage.removeItem('Countdown.htm-countdown');
+    window.localStorage.removeItem('Countdown.htm-score');
+}
+
+var interval = 0;
 var running = true;
 
 window.onload = function(e){
-    var interval = window.setInterval(
+    document.getElementById('countdown').innerHTML =
+      window.localStorage.getItem('Countdown.htm-countdown')
+      || 10;
+    document.getElementById('score').innerHTML =
+      window.localStorage.getItem('Countdown.htm-score')
+      || 0;
+
+    interval = window.setInterval(
       'countdown()',
       1000
     );
