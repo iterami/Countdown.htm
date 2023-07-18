1e444c6fdde0759d28b5eed70250fc35bc68343d
Jan Hořava <czechinthemail@gmail.com>
2023-07-18 13:06:33 -0700

    combined js into repo.js

--- repo.js
+++ repo.js
@@ -22,3 +22,31 @@ function countdown(){
 
     core_storage_update();
 }
+
+function repo_init(){
+    core_repo_init({
+      'events': {
+        'add': {
+          'onclick': function(){
+              add(100);
+          },
+        },
+      },
+      'globals': {
+        'running': true,
+      },
+      'storage': {
+        'countdown': 10,
+        'score': 0,
+      },
+      'title': 'Countdown.htm',
+    });
+
+    core_storage_update();
+
+    core_interval_modify({
+      'id': 'countdown',
+      'interval': 1000,
+      'todo': countdown,
+    });
+}
