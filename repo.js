f9b3c10d42adec85c1816fd3be7dab6c451136a5
Jan Hořava <czechinthemail@gmail.com>
2026-02-20 08:14:51 -0800

    added start button, locked interval, and moved handling to functions

--- repo.js
+++ repo.js
@@ -1,5 +1,28 @@
 'use strict';
 
+function add(event){
+    if(core_intervals.countdown.paused
+      || core_storage_data.countdown <= 0){
+        return;
+    }
+
+    event.target.blur();
+    core_storage_data.countdown += core_storage_data.added;
+    core_storage_update();
+}
+
+function interval(){
+    if(core_storage_data.countdown <= 0){
+        core_interval_lock('countdown');
+        return;
+    }
+
+    core_storage_data.countdown -= 1;
+    core_storage_data.score += 1;
+
+    core_storage_update();
+}
+
 function repo_init(){
     core_repo_init({
       'beforeunload': {
@@ -7,18 +30,13 @@ function repo_init(){
       },
       'events': {
         'add': {
-          'onclick': function(event){
-              if(core_intervals.countdown.paused
-                || core_storage_data.countdown <= 0){
-                  return;
-              }
-
-              event.target.blur();
-              core_storage_data.countdown += core_storage_data.added;
-              core_storage_update();
-          },
+          'onclick': add,
+        },
+        'start': {
+          'onclick': start,
         },
       },
+      'info': '<button class=medium id=start type=button>Start New Game</button>',
       'storage': {
         'added': 100,
         'countdown': 10,
@@ -30,22 +48,26 @@ function repo_init(){
       'title': 'Countdown.htm',
     });
 
+    start(true);
+}
+
+function start(init){
+    if(init !== true){
+        if(!globalThis.confirm('Start new game?')){
+            return;
+        }
+
+        core_storage_data.countdown = 10;
+        core_storage_data.score = 0;
+        core_escape(false);
+    }
+
     core_storage_update();
     document.getElementById('add').textContent = '+' + core_storage_data.added;
 
     core_interval_modify({
       'id': 'countdown',
       'interval': core_storage_data.interval,
-      'todo': function(){
-          if(core_storage_data.countdown <= 0){
-              core_interval_pause_all();
-              return;
-          }
-
-          core_storage_data.countdown -= 1;
-          core_storage_data.score += 1;
-
-          core_storage_update();
-      },
+      'todo': interval,
     });
 }
